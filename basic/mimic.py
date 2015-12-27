e211c989e101a3296c6292c9f1997d4728e2da9e
João Luiz Lorencetti <me@dirtycoder.net>
2015-12-27 14:49:45 -0200

    Solves mimic.py

--- basic/mimic.py
+++ basic/mimic.py
@@ -46,26 +46,65 @@ import sys
 
 
 def mimic_dict(filename):
-  """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
+    """Returns mimic dict mapping each word to list of words which follow it."""
+    mimic = dict()
+
+    with open(filename) as f:
+        text = f.read().lower()
+    words = text.split()
+
+    for idx, word in enumerate(words):
+        last_word = len(words) == idx + 1
+
+        if not mimic.get(word):
+            mimic[word] = []
+
+        if not last_word:
+            mimic[word].append(words[idx + 1])
+
+    return mimic
+
+
+def get_random_word(mimic_dict, word):
+    """Get a random word, keep trying until it has a valid list of choices"""
+    while not mimic_dict.get(word):
+        word = random.choice(list(mimic_dict.keys()))
+
+    word = random.choice(mimic_dict.get(word))
+
+    return word
+
+
+def append_text(words, word):
+    """Append the new word to the list of words and add new lines if needed"""
+    text = ' '.join(words)
+
+    if len(text.split('\n')[-1]) > 70:
+        words.append('\n')
+
+    words.append(word)
 
 
 def print_mimic(mimic_dict, word):
-  """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
+    """Given mimic dict and start word, prints 200 random words."""
+    words = []
+
+    for i in range(0, 200):
+        word = get_random_word(mimic_dict, word)
+        append_text(words, word)
+
+    print(' '.join(words))
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
-  if len(sys.argv) != 2:
-    print('usage: ./mimic.py file-to-read')
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print('usage: ./mimic.py file-to-read')
+        sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
+    dict = mimic_dict(sys.argv[1])
+    print_mimic(dict, '')
 
 
 if __name__ == '__main__':
-  main()
+    main()
